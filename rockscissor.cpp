b74f7f9b1e5da102fed0b530dd151df5985ad040
missonpaudel <missonpaudel103@gmail.com>
2024-07-09 18:20:47 +0545

    complttion of rockscissor

--- rockscissor.cpp
+++ rockscissor.cpp
@@ -12,10 +12,14 @@ int main (){
     char computer;
 
     player = getUserChoice();
-    std::cout << " your choice: ";
+    std::cout << " Your choice: ";
     showChoice(player);
 
     computer = getComputerChoice();
+    std::cout << " Computer's choice: ";
+    showChoice(computer);
+
+    chooseWinner(player,computer);
 
     return 0;
 }
@@ -37,20 +41,71 @@ return 0;
 }
 char getComputerChoice(){
 
-return 0;
+    srand(time(0));
+    int num = (rand()%3)+1;
+    char computer;
+
+    switch (num){
+        case 1: computer = 'r';
+            break;
+        case 2: computer = 'p';
+            break;
+        case 3: computer = 's';
+            break;
+    }
+
+return computer;
 }
 void showChoice(char choice){
     switch(choice){
-        case 'r': std::cout << "ROCK";
+        case 'r': std::cout << "ROCK \n";
             break;
-        case 'p': std::cout <<"PAPER";
+        case 'p': std::cout <<"PAPER \n";
             break;
-        case 's': std::cout << "SCISSORS";
+        case 's': std::cout << "SCISSORS \n";
     }
 
 
 }
 void chooseWinner(char player,char computer){
+switch(player){
+    case 'r': 
+        if(computer == 'r'){
+            std::cout << "It's a tie";
+        }
+        else if (computer == 'p'){
+            std::cout << "You loose";
+        }
+        else {
+            std::cout <<"YOOU WIN!!";
+        }
+    break;
 
+
+    case 'p': 
+        if(computer == 'p'){
+            std::cout << "It's a tie";
+        }
+        else if (computer == 's'){
+            std::cout << "You loose";
+        }
+        else {
+            std::cout <<"YOOU WIN!!";
+        }
+    break;
+
+
+    case 's': 
+        if(computer == 's'){
+            std::cout << "It's a tie";
+        }
+        else if (computer == 'r'){
+            std::cout << "You loose";
+        }
+        else {
+            std::cout <<"YOOU WIN!!";
+        }
+    break;
+}
 
 }
